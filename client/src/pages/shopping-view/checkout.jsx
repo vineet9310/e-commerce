acdb791d1e4e1beff6a34bfad69325d3eb914b68
vineet <vineetbeniwal9310@gmail.com>
2025-03-03 12:43:01 +0530

    commit

---
+++ client/src/pages/shopping-view/checkout.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ShopppingCheckout() {
+  return (
+    <div>Shopping view Checkout</div>
+  )
+}
+
+export default ShopppingCheckout
---
+++ client/src/pages/shopping-view/home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ShoppingHome() {
+  return (
+    <div>Shopping view home</div>
+  )
+}
+
+export default ShoppingHome
