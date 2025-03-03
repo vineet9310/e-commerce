9c609de44cbfc5701b584c72189a74203f6dbbb4
vineet <vineetbeniwal9310@gmail.com>
2025-03-03 18:17:40 +0530

    commit

--- client/src/components/common/form.jsx
+++ client/src/components/common/form.jsx
@@ -1,9 +1,70 @@
-import React from 'react'
+import React from "react";
+import { Label } from "../ui/label";
+// import { get } from 'mongoose';
+import { Input } from "../ui/input";
 
-function form() {
+const types = {
+  INPUT: "input",
+  SELECT: "select",
+  TEXTAREA: "textarea",
+};
+
+function CommonForm({ formControls }) {
+  function renderInputsByComponentType(getControlItem) {
+    let element = null;
+    switch (getControlItem.componentType) {
+      case "input":
+        element = (
+          <Input
+            name={getControlItem.name}
+            placeholder={getControlItem.placeholder}
+            id={getControlItem.name}
+            type={getControlItem.type}
+          />
+        );
+
+        break;
+      case "select":
+        element = (
+          <Input
+            name={getControlItem.name}
+            placeholder={getControlItem.placeholder}
+            id={getControlItem.name}
+            type={getControlItem.type}
+          />
+        );
+
+        break;
+      case "textarea":
+        element = (
+          <Input
+            name={getControlItem.name}
+            placeholder={getControlItem.placeholder}
+            id={getControlItem.name}
+            type={getControlItem.type}
+          />
+        );
+
+        break;
+
+      default:
+        break;
+    }
+    return element;
+  }
+
   return (
-    <div>form</div>
-  )
+    <form>
+      <div className="flex flex-col gap-3">
+        {formControls.map((controlItem) => (
+          <div className="grid w-full gap-1.5" key={controlItem.name}>
+            <Label className="mb-1">{controlItem.Label}</Label>
+            {renderInputsByComponentType(controlItem)}
+          </div>
+        ))}
+      </div>
+    </form>
+  );
 }
 
-export default form
+export default CommonForm;
